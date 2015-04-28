d8c3268b598a20b09854f53077bd5352772e875a
Dave Loyall <david.loyall@nebraska.gov>
2015-04-28 11:37:01 -0500

    more removed than added!

--- src/main/java/us/ne/state/ocio/publichealth/utilities/ToxiPhinmsBridge.java
+++ src/main/java/us/ne/state/ocio/publichealth/utilities/ToxiPhinmsBridge.java
@@ -1,15 +1,11 @@
 package us.ne.state.ocio.publichealth.utilities;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.DirectoryStream.Filter;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Comparator;
-import java.util.TreeSet;
+import java.util.regex.Pattern;
 
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
@@ -21,10 +17,10 @@ public class ToxiPhinmsBridge {
 	private static final Logger log = LogManager.getLogger(ToxiPhinmsBridge.class);
 
 	public static void main(String[] args) throws IOException {
-		ToxiPhinmsBridge shb = new ToxiPhinmsBridge();
-		JCommander jc = new JCommander(shb, args);
+		ToxiPhinmsBridge tpb = new ToxiPhinmsBridge();
+		JCommander jc = new JCommander(tpb, args);
 		jc.setProgramName("java -jar toxiPhinmsBridge.jar");
-		if (shb.run() == 42)
+		if (tpb.run() == 42)
 			jc.usage();
 	}
 
@@ -37,55 +33,37 @@ public class ToxiPhinmsBridge {
 	@Parameter(names = "--output", description = "Toxicall bidirectional directory")
 	private String toxiDirectory;
 
-	@Parameter(names = "--archive", description = "Archive directory")
-	private String archive;
-
 	@Parameter(names = "--cooldown", description = "Ignore files modified less than [cooldown] seconds ago. (This avoids partial files.)")
 	private int coolDownInSeconds = 30;
 
 	@Parameter(names = "--help", help = true, description = "This help message.", hidden = true)
 	private boolean help;
 
+	private static final String sFiles = "^.*[.](ts[fs]|tdf)([.]zip)?";  //We're going to pass Pattern.CASE_INSENSITIVE when we compile these.
+	private static final String aFiles = "^.*[.](ta[fs]|tdf)([.]zip)?";
+	
 	private int run() throws IOException {
-		if (help || anyNull(input, toxiDirectory, archive)) {
+		if (help || anyNull(input, toxiDirectory, outgoing)) {
 			return 42; //magic number tells caller to display usage;
 		}
 
-		Filter<Path> filter = new CoolDownFilterWithoutDirectories<Path>(coolDownInSeconds); //defined in this file.
+		Filter<Path> aFilesFilter = new CoolDownFilterWithoutDirectories<Path>(coolDownInSeconds,aFiles); 
+		Filter<Path> sFilesFilter = new CoolDownFilterWithoutDirectories<Path>(coolDownInSeconds,sFiles); 
 		
-		TreeSet<Path> files = new TreeSet<Path>(new SlowComparator<Path>()); //defined in this file.
+		//These two loops take care of the PCC side...
+		//   ...And the DOH side.  Cool.  the --input directory means one thing on PCC and another on DOH...
 		
-	    log.info("Preparing file list...");
-		for(Path p : Files.newDirectoryStream(Paths.get(input), filter)) {
-	        files.add(p);
-	    }
-		log.info("Found {} suitable files in {}.",files.size(),input);
-		
-		int inputCounter = 0;
-		int outputCounter = 0;
-		Path outputPath = null;
-		long batchNumber = 0;
-		
-		Path archivePath = null;
-		for (Path p : files) {
-			if(inputCounter >= batchSize) inputCounter = 0;
-			if(inputCounter == 0) {
-				outputCounter++;
-				batchNumber = System.currentTimeMillis();
-				archivePath = Files.createDirectory(Paths.get(archive + FileSystems.getDefault().getSeparator() + batchNumber));
-				outputPath=getNewOutputFile(batchNumber);
-			}
-			Files.write(outputPath, Files.readAllLines(p, StandardCharsets.UTF_8), StandardCharsets.UTF_8,
-		            StandardOpenOption.CREATE, StandardOpenOption.APPEND);
-			Files.move(p, archivePath.resolve(p.getFileName()));
-			inputCounter++;
+		for (Path p : Files.newDirectoryStream(Paths.get(toxiDirectory), sFilesFilter)) {
+			log.info("Attempting to move {} to {}.",p,outgoing);
+			Files.move(p, Paths.get(outgoing).resolve(p.getFileName()));
 		}
-		log.debug("created {} batch files in {}.",outputCounter,toxiDirectory);
+		
+		for (Path p : Files.newDirectoryStream(Paths.get(input), aFilesFilter)) {
+			log.info("Attempting to move {} to {}.",p,toxiDirectory);
+			Files.move(p, Paths.get(toxiDirectory).resolve(p.getFileName()));
+		}
+		
 		return 0;
-	}
-
-	private Path getNewOutputFile(long batchNumber) {
-		return Paths.get(toxiDirectory + FileSystems.getDefault().getSeparator() + "batch." + batchNumber + ".hl7");
 	}
 
 	private static boolean anyNull(Object... args) {
@@ -98,32 +76,21 @@ public class ToxiPhinmsBridge {
 	
 	class CoolDownFilterWithoutDirectories<T> implements Filter<Path> {
 		int coolDownInSeconds;
+		Pattern p;
 		
-		public CoolDownFilterWithoutDirectories(int coolDownInSeconds) {
+		public CoolDownFilterWithoutDirectories(int coolDownInSeconds, String regexFilter) {
 			this.coolDownInSeconds = coolDownInSeconds;
+			this.p = Pattern.compile(regexFilter,Pattern.CASE_INSENSITIVE); 
 		}
 
 		@Override
 		public boolean accept(Path entry) throws IOException {
-			return !Files.isDirectory(entry) && Files.getLastModifiedTime(entry).toMillis() < System.currentTimeMillis() - coolDownInSeconds * 1000;
+			boolean filterResult = p.matcher(entry.getFileName().toString()).matches();
+			boolean coolDownResult = Files.getLastModifiedTime(entry).toMillis() < System.currentTimeMillis() - coolDownInSeconds * 1000;
+			return !Files.isDirectory(entry) && coolDownResult && filterResult; 
 		}
 
 	}
 	
-	//TODO it's slow (on 660 files).
-	class SlowComparator<T> implements Comparator<Path> {
-		@Override
-		public int compare(Path o1, Path o2) {
-	        try {
-	        	String timePlusName1 = Files.getLastModifiedTime(o1) + o1.getFileName().toString();
-	        	String timePlusName2 = Files.getLastModifiedTime(o2) + o2.getFileName().toString();
-	            return timePlusName1.compareTo(timePlusName2);
-	        } catch (IOException e) {
-	            log.error("Sorting file list failed.",e);
-	        }
-	        return 0;
-	    }
-
-	}		
 }
 
